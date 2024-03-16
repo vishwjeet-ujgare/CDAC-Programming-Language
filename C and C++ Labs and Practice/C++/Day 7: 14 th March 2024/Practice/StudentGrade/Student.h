092af234e81b0f970e49a74c2a6ba7f91f850c03
vishwjeet <vrvishwujgare@gmail.com>
2024-03-16 18:24:05 +0530

    student new code

--- C and C++ Labs and Practice/C++/Day 7: 14 th March 2024/Practice/StudentGrade/Student.h
+++ C and C++ Labs and Practice/C++/Day 7: 14 th March 2024/Practice/StudentGrade/Student.h
@@ -1,54 +1,45 @@
 #include <iostream>
 #include <string.h>
-using namespace  std;
+using namespace std;
 
-
-class Student{
+class Student
+{
 public:
-    int id=1;
+    int id;
     char name[10];
     // string name;
     float per;
     char grade;
     int marks[5];
     int totalMarks;
-    int totalSubject=5;
+    int totalSubject = 5;
 
-void diplayStudentDetails(Student &s)
-{   
-    cout<<"ID : "<<s.id<<endl;
-    cout<<"Name :"<<s.name<<endl;
-    cout<<"percentage :"<<s.per<<endl;
-    cout<<"grade :"<<s.grade<<endl;
-}
-
-
-void userInput(Student &s)
-{
-
-
-   float userInput;
-   cout<<"Enter Student Details"<<endl;;
-
-   s.id=id;
-   s.totalSubject=totalSubject;
-
-  
-
-    cout<<"Enter Student Name : ";
-    cin>>s.name;
-
-    cout<<"Enter Student Marks for "<<s.
-    totalSubject<<" subjects"<<endl;
-    for(int i=0;i<s.totalSubject;i++)
+    void diplayStudentDetails(Student &s)
     {
-        cout<<i<<". ";
-        cin>>s.marks[i];
-        cout<<endl;
+        cout<<"----------------------------"<<endl;
+        cout << "ID : " << s.id << endl;
+        cout << "Name :" << s.name << endl;
+        cout << "percentage :" << s.per << endl;
+        cout << "grade :" << s.grade << endl;
     }
-    
-}
 
+    void userInput(Student &s)
+    {
+
+   
+        s.totalSubject = totalSubject;
+
+        cout << endl;
+        cout << "Enter details for student id : " << s.id << endl;
+        cout << "Enter Student Name : ";
+        cin >> s.name;
+
+        cout << "Enter Student Marks for " << s.totalSubject << " subjects" << endl;
+        for (int i = 0; i < s.totalSubject; i++)
+        {
+            cout << i+1 << ". ";
+            cin >> s.marks[i];
+            cout << endl;
+        }
+    }
 };
-
-
--- C and C++ Labs and Practice/C++/Day 7: 14 th March 2024/Practice/StudentGrade/main.cpp
+++ C and C++ Labs and Practice/C++/Day 7: 14 th March 2024/Practice/StudentGrade/main.cpp
@@ -7,18 +7,47 @@ using namespace  std;
 
 int main()
 {
-    Student s1;
-    Student s;
-    Result r;
+    
+ 
+       int totalStudents;
 
-    s.userInput(s);
+        cout << "Enter Student number : ";
+        cin >> totalStudents;
+
+        Student students[totalStudents];
+
+        cout << endl;
+        cout << "======Enter Student Details for" << totalStudents<<"====="<< endl;
+        ;
+
+    for(int i=0;i<totalStudents;i++)
+    {
+        Student s;
+        s.id=i+1;
+        s.userInput(s);
+        students[i]=s;
+
+    }
+
+
+
+cout<<"==============Displaying students Details========================"<<endl;
    
-    // s.name="vishwjeet";
-    r.calcMarks(s);
-    r.calcPer(s);
-    r.calculateGrade(s);
-    s.diplayStudentDetails(s);
+   
+    for(int i=0;i<totalStudents;i++)
+    {
 
+        Result r;
+       Student s1= students[i];
+
+    r.calcMarks(s1);
+    r.calcPer(s1);
+    r.calculateGrade(s1);
+    s1.diplayStudentDetails(s1);
+    }
+
+    
+   
 
 
 }
